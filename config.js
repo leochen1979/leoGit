f1893becb829155cf56c7907860358b762708691
“leochen1979” <570372661@qq.com>
2021-01-28 06:34:48 -0500

    del

---
+++ config.js
@@ -0,0 +1,7 @@
+let config = {
+  host    : '127.0.0.1',
+  user    : 'root',
+  password: '123456',
+  database: 'leo_db'
+};
+
---
+++ index.js
@@ -0,0 +1,25 @@
+var mysql = require('mariadb');
+
+var con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "123456",
+        port: 3306
+        })
+        .then(conn => {
+                console.log("connected" + conn.threadId);
+                
+                conn.query("SELECT * from leo_db.orders")
+                .then((rows) => {
+                  console.log(rows); 
+               
+                })
+
+
+
+
+
+        })
+        .catch(err => {
+                console.log("got error " + err);
+        });
